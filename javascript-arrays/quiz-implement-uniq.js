9eda9081bfe6971d89edaa6ee80a051bcccbe29d
xaviguasch <xaviguasch@gmail.com>
2023-01-22 13:49:01 +0100

    added problem javascript-arrays quiz-implement-uniq

---
+++ javascript-arrays/quiz-implement-uniq.js
@@ -0,0 +1,11 @@
+// Quiz: "Implement uniq"
+// This quiz includes topics from these lessons: "For each", "Includes", "Stack". If you find this quiz difficult, revisiting those lessons may help!
+// Write a function uniq(arr). It should return a new array without any duplicate values. It should not change the original array.
+
+function uniq(arr) {
+  return arr
+}
+
+console.log(uniq([1, 2, 3]))
+console.log(uniq(['a', 'a', 'b']))
+console.log(uniq([1, 1, 1, 1]))
