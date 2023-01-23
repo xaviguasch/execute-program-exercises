b304a9d07106900cebbc68a41546373c994e4536
xaviguasch <xaviguasch@gmail.com>
2023-01-23 18:30:31 +0100

    added problem javascript-arrays quiz-all-true

---
+++ javascript-arrays/quiz-all-true.js
@@ -0,0 +1,17 @@
+// Quiz: "All true"
+// This quiz includes topics from this lesson: "Reduce". If you find this quiz difficult, revisiting that lesson may help!
+// Write a function that returns true if all array elements are true. Use reduce.
+
+// (Mind the difference between JavaScript's == and ===.)
+
+const allTrue = (values) => {
+  return
+}
+
+console.log(allTrue([true])) // true
+console.log(allTrue([false])) // false
+console.log(allTrue([true, false])) // false
+console.log(allTrue([false, true])) // false
+console.log(allTrue([false, false])) // false
+console.log(allTrue([true, true])) // true
+console.log(allTrue([])) // true
