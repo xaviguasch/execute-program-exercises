8f38aebf7786d837a9a321e72ae8377a60fd3393
xaviguasch <xaviguasch@gmail.com>
2023-01-24 17:20:40 +0100

    added problem javascript-arrays quiz-implement-filter-with-for

---
+++ javascript-arrays/quiz-implement-filter-for.js
@@ -0,0 +1,13 @@
+// Quiz: "Implement Filter With For"
+// This quiz includes topics from these lessons: "For each", "Filter". If you find this quiz difficult, revisiting those lessons may help!
+// Use forEach to write a function that behaves like filter.
+
+const filter = (arr, callback) => {
+  return
+}
+
+console.log(filter([1, 2, 3], (num) => num >= 0)) // [1,2,3]
+console.log(filter([1, 2, 3], (num) => num > 1)) // [2,3]
+console.log(filter([1, 2, 3], (num) => num > 5)) // []
+console.log(filter([null, undefined], (num) => num >= true)) // [null, undefined]
+console.log(filter([], (num) => num >= true)) // []
