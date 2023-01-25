8a41e079ce40d4f939dd212d33f8554fcf90339c
xaviguasch <xaviguasch@gmail.com>
2023-01-25 21:07:02 +0100

    added problem javascript-arrays quiz-even-numbers

---
+++ javascript-arrays/quiz-even-numbers.js
@@ -0,0 +1,16 @@
+// Quiz: "Even numbers"
+// This quiz includes topics from this lesson: "Filter". If you find this quiz difficult, revisiting that lesson may help!
+// Write a function that returns only the even numbers from an array.
+
+// This will require a modulus operator, %. You can think about x % y as "the remainder when dividing x by y." For example, 1 % 2 === 1, 2 % 2 === 0, and 3 % 2 === 1.
+
+// x % 2 === 0 means that a number is even.
+
+const even = (arr) => {
+  return [2, 4]
+}
+
+console.log(even([1, 2, 3, 4])) // [2,4]
+console.log(even([1, 2, 3])) // [2]
+console.log(even([2, 4, 6, 8])) // [2,4,6,8]
+console.log(even([])) // []
