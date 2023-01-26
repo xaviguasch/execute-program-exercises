832b690d7f1afb6ca0c6218fa19b1b621daf225e
xaviguasch <xaviguasch@gmail.com>
2023-01-26 20:11:25 +0100

    added problem javascript-arrays quiz-sum-squares-of-odds

---
+++ javascript-arrays/quiz-sum-squares-of-odds.js
@@ -0,0 +1,19 @@
+// Quiz: "Sum squares of odds"
+// This quiz includes topics from these lessons: "Map", "Filter", "Reduce". If you find this quiz difficult, revisiting those lessons may help!
+// Sum the squares of the odd numbers in an array. Use map, filter, and reduce together.
+
+// A hint: x is odd if x % 2 === 1.
+
+const sumSquaresOfOdds = (arr) => {
+  return
+}
+
+console.log(sumSquaresOfOdds([0, 0])) // 0
+console.log(sumSquaresOfOdds([3])) // 9
+console.log(sumSquaresOfOdds([4])) // 0
+console.log(sumSquaresOfOdds([5])) // 25
+console.log(sumSquaresOfOdds([1, 2])) // 1
+console.log(sumSquaresOfOdds([1, 2, 3])) // 10
+console.log(sumSquaresOfOdds([1, 2, 3, 4, 5])) // 35
+console.log(sumSquaresOfOdds([2, 2, 2, 2])) // 0
+console.log(sumSquaresOfOdds([])) // 0
