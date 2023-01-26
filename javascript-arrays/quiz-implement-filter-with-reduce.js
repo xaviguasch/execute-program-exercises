8392ec2e426cf1537a3ada0027d22254b2044c4b
xaviguasch <xaviguasch@gmail.com>
2023-01-26 20:00:48 +0100

    added problem javascript-arrays quiz-implement-filter-with-reduce

---
+++ javascript-arrays/quiz-implement-filter-with-reduce.js
@@ -0,0 +1,18 @@
+// Quiz: "Implement Filter With Reduce"
+// This quiz includes topics from these lessons: "Reduce", "Filter", "Concat". If you find this quiz difficult, revisiting those lessons may help!
+// Use reduce to write a function that behaves like filter. You can do this with only one reduce and no other loops.
+
+// Two tips:
+
+// Remember to return acc inside your reduce callback.
+// Remember to pass an initial value of [] as reduce's second argument.
+
+const filter = (arr, callback) => {
+  return
+}
+
+console.log(filter([1, 2, 3], (num) => num >= 0)) // [1,2,3]
+console.log(filter([1, 2, 3], (num) => num > 1)) // [2,3]
+console.log(filter([1, 2, 3], (num) => num > 5)) // []
+console.log(filter([null, undefined], (num) => true)) // [null, undefined]
+console.log(filter([], (num) => true)) // []
