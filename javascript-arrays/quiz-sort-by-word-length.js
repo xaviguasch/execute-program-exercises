0cd9310787c3585b568227c04787c4ad8c7a42ee
xaviguasch <xaviguasch@gmail.com>
2023-01-27 16:25:38 +0100

    added problem javascript-arrays quiz-sort-by-word-length

---
+++ javascript-arrays/quiz-sort-by-word-length.js
@@ -0,0 +1,15 @@
+// Quiz: "Sort by word length"
+// This quiz includes topics from these lessons: "Sort", "Slice". If you find this quiz difficult, revisiting those lessons may help!
+// Write a function that sorts words by length in ascending order.
+
+// (Note: We call your sortByLength function on the array, but we don't use your function's return value. Instead, your function should modify the array in-place. That's what JavaScript's array sort function does, so you don't need to do anything special! But if you copy the array with a method like slice, sort the copy, and then return the sorted copy, that won't work.)
+
+const sortByLength = (words) => {
+  return
+}
+
+console.log(sortByLength([])) // []
+console.log(sortByLength(['z'])) // ['z']
+console.log(sortByLength(['a', 'b'])) // 'a', 'b'][]
+console.log(sortByLength(['a', 'big', 'ox'])) // ['a', 'ox', 'big']
+console.log(sortByLength(['zzz', 'yy', 'x'])) // ['x', 'yy', 'zzz']
