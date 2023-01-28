0eac3b60606cd607b8f758252b3afa22eab2da2e
xaviguasch <xaviguasch@gmail.com>
2023-01-28 16:47:49 +0100

    added problem javascript-arrays quiz-implement-find

---
+++ javascript-arrays/quiz-implement-find.js
@@ -0,0 +1,13 @@
+// Quiz: "Implement find"
+// This quiz includes topics from these lessons: "Find index", "Find", "Negative array indexes". If you find this quiz difficult, revisiting those lessons may help!
+// Use findIndex to write the function find(arr, callback). It should return the first element where callback(element) is true. If the element is not found, it should return undefined.
+
+const find = (arr, callback) => {
+  return
+}
+
+console.log(find([1, 2, 3], (n) => n === 1)) // 1
+console.log(find([5, 6, 7], (n) => n / 2 === 3)) // 6
+console.log(find([1, 2, 3], (n) => n > 5)) // undefined
+console.log(find([], (n) => n > 5)) // undefined
+console.log(find([null], (n) => n === null)) // null
