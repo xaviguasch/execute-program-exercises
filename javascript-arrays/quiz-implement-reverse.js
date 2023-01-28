c928c00eed4457c65de97c19cdb5f1c3cb477387
xaviguasch <xaviguasch@gmail.com>
2023-01-28 17:00:14 +0100

    added problem javascript-arrays quiz-implement-reverse

---
+++ javascript-arrays/quiz-implement-reverse.js
@@ -0,0 +1,12 @@
+// Quiz: "Implement Reverse"
+// This quiz includes topics from these lessons: "Reverse", "Reduce right", "Concat". If you find this quiz difficult, revisiting those lessons may help!
+// Write the function reverse using reduceRight. A hint: use concat in your reduce callback function.
+
+const reverse = (arr) => {
+  return
+}
+
+console.log(reverse([1])) // 1
+console.log(reverse([1, 2, 3, 4, 5])) // [5, 4, 3, 2, 1]
+console.log(reverse(['a', 'b', 'c'])) // ['c', 'b', 'a']
+console.log(reverse([])) // []
