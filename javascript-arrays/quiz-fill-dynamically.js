cc3435979d22700c2e623d7b25aa757a8e2ed28b
xaviguasch <xaviguasch@gmail.com>
2023-03-11 18:13:07 +0100

    added solution to previous exercise

--- javascript-arrays/quiz-fill-dynamically.js
+++ javascript-arrays/quiz-fill-dynamically.js
@@ -1,7 +1,7 @@
 // Write a function fillDynamically(value, length). It should construct a new array of length filled with value
 
 const fillDynamically = (value, length) => {
-  return
+  return new Array(length).fill(value)
 }
 
 console.log(fillDynamically('d', 3)) // ['d', 'd', 'd']
