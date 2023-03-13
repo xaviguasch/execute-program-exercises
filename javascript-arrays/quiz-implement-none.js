a59ffa5ffdb9359389afc6c432f54ceb530f5ae3
xaviguasch <xaviguasch@gmail.com>
2023-03-13 17:29:53 +0100

    added problem javascript-arrays quiz-implement-none

---
+++ javascript-arrays/quiz-implement-none.js
@@ -0,0 +1,11 @@
+// Quiz: "Implement none"
+// This quiz includes topics from this lesson: "Some and every". If you find this quiz difficult, revisiting that lesson may help!
+// Write a function none(arr, callback). It should return true if the callback returns false for every element.
+
+const none = (arr, callback) => {
+  return
+}
+
+console.log(none([1, 2, 4], (n) => n === 3))
+console.log(none([1, 2], (n) => n === 2))
+console.log(none(['a', 'bc', 'def'], (s) => s.length === 0))
