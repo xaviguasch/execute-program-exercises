6c549bed7e41b45ea437afb74fff2cd30311ceac
xaviguasch <xaviguasch@gmail.com>
2023-03-13 17:34:09 +0100

    added problem javascript-arrays quiz-has-null

---
+++ javascript-arrays/quiz-has-null.js
@@ -0,0 +1,13 @@
+// Quiz: "Has null"
+// This quiz includes topics from this lesson: "Some and every". If you find this quiz difficult, revisiting that lesson may help!
+// Write a function hasNull that returns true if an array contains null.
+
+// A hint: null == undefined (this is a "double equals" comparison). But null !== undefined (this is a "triple equals" comparison).
+
+const hasNull = (arr) => {
+  return
+}
+
+console.log(hasNull([null, null]))
+console.log(hhasNull([1, null, 3]))
+console.log(hasNull([1, 2, 3]))
