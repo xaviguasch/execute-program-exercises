8e219ee24b299339f44dd1f9c72d915d491da15c
xaviguasch <xaviguasch@gmail.com>
2023-03-23 16:02:24 +0100

    added t02-quiz-pluralize ts exercise

---
+++ typescript-basics/t02-quiz-pluralize/quiz-pluralize.ts
@@ -0,0 +1,10 @@
+// Quiz: "Pluralize"
+// This quiz includes topics from this lesson: "Functions". If you find this quiz difficult, revisiting that lesson may help!
+// Write a function that makes words plural by adding "s". Remember that '+' joins two strings.
+
+function pluralize() {}
+
+console.log(pluralize('cat'))
+console.log(pluralize('dog'))
+console.log(pluralize(null))
+console.log(pluralize(5))
