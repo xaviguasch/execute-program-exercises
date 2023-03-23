25c56d09e78398a0785166340f4c9ef764066635
xaviguasch <xaviguasch@gmail.com>
2023-03-23 16:53:35 +0100

    added solution to previous challenge

--- typescript-basics/t04-using-operators/using-operators.js
+++ typescript-basics/t04-using-operators/using-operators.js
@@ -1,11 +1,9 @@
 // Quiz: "Using operators"
 // This quiz includes topics from these lessons: "Functions", "Operators". If you find this quiz difficult, revisiting those lessons may help!
 // Write a function that adds two numbers using +. It should type error when passed strings.
-
-function add(a: number, b: number): number {
-  return a + b
+function add(a, b) {
+    return a + b;
 }
-
 // console.log(add('1', '1'))
-console.log(add(1, 1))
-console.log(add(5, 10))
+console.log(add(1, 1));
+console.log(add(5, 10));
