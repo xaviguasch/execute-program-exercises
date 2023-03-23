1167477820a729f89d09754e5632e703c7c692ba
xaviguasch <xaviguasch@gmail.com>
2023-03-23 20:21:02 +0100

    added t07-quiz-stringify ts challenge

---
+++ typescript-basics/t07-quiz-stringify/stringify.ts
@@ -0,0 +1,13 @@
+// Quiz: "Stringify"
+// This quiz includes topics from these lessons: "Functions", "Type unions", "Return type inference". If you find this quiz difficult, revisiting those lessons may help!
+// Write a function that returns its argument as a string. It should take a string, a number, or a boolean and return a string. (Remember that .toString() works on these types)
+
+function stringify() {
+  return
+}
+
+console.log(stringify('hello'))
+console.log(stringify(5))
+console.log(stringify(false))
+console.log(stringify(null))
+console.log(stringify({ email: 'a@example.com' }))
