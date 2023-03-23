4dbc117c115633474244f0120780f4bb11d810bb
xaviguasch <xaviguasch@gmail.com>
2023-03-23 18:29:59 +0100

    added t05-quiz-array-identity ts exercise

---
+++ typescript-basics/t05-quiz-array-identity/array-identity.ts
@@ -0,0 +1,13 @@
+// Quiz: "Array identity"
+// This quiz includes topics from these lessons: "Functions", "Arrays". If you find this quiz difficult, revisiting those lessons may help!
+// Write a function that takes an array of numbers, then returns it. It shouldn't change the array in any way. Force the array to contain numbers, so that other types of arrays are type errors.
+
+function f() {
+  return
+}
+
+console.log(f([]))
+console.log(f([1]))
+console.log(f(['1']))
+console.log(f(null))
+console.log(f(undefined))
