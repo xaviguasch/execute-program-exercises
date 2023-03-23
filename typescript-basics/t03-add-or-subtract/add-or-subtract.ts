adb32a662bec26a92969935096779bc4b728ad9c
xaviguasch <xaviguasch@gmail.com>
2023-03-23 16:30:02 +0100

    added t03-add-or-subtract ts exercise

---
+++ typescript-basics/t03-add-or-subtract/add-or-subtract.ts
@@ -0,0 +1,9 @@
+// Quiz: "Add or subtract"
+// This quiz includes topics from this lesson: "Functions". If you find this quiz difficult, revisiting that lesson may help!
+// Write a function that adds or subtracts 1 from a number. Argument 1 is the number. Argument 2 is a boolean. When it's true, add; otherwise, subtract.
+
+function addOrSubtract() {}
+
+console.log(addOrSubtract(5, true))
+console.log(addOrSubtract('5', true))
+console.log(addOrSubtract('5', null))
