767d9c02729a05bbcb7e552ba896bf534e8e4b0e
xaviguasch <xaviguasch@gmail.com>
2023-03-23 15:51:40 +0100

    added solution to previous challenge

---
+++ typescript-basics/t01-quiz-type-error/type-error.js
@@ -0,0 +1,2 @@
+// The code below contains a type error: we're trying to assign a number to a variable of type string. Fix the type so that the code can compile and run.
+var someNumber = 5;
--- typescript-basics/t01-quiz-type-error/type-error.ts
+++ typescript-basics/t01-quiz-type-error/type-error.ts
@@ -1,3 +1,3 @@
 // The code below contains a type error: we're trying to assign a number to a variable of type string. Fix the type so that the code can compile and run.
 
-const someNumber: string = 5
+const someNumber: number = 5
