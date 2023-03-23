f8007984ddd6d8841227ab98ed2f970fd35260da
xaviguasch <xaviguasch@gmail.com>
2023-03-23 18:53:40 +0100

    added solution to previous challenge

--- typescript-basics/t06-quiz-syntax-error/syntax-error.js
+++ typescript-basics/t06-quiz-syntax-error/syntax-error.js
@@ -1,7 +1,7 @@
 // Quiz: "Syntax error"
 // This quiz includes topics from these lessons: "Functions", "Syntax errors vs type errors". If you find this quiz difficult, revisiting those lessons may help!
 // Change the function to have invalid syntax.
-
 function f() {
-  @#*&%
+     *  &  %
+    ;
 }
