08003a02ed2fb899b1a2983363efe1be778ab2f2
Xavi Guasch <xaviguasch@gmail.com>
2023-03-24 17:26:22 +0100

    added solution to previous challenge

--- typescript-basics/t09-quiz-writing-a-function-type/writing-a-function-type.js
+++ typescript-basics/t09-quiz-writing-a-function-type/writing-a-function-type.js
@@ -1,23 +1,12 @@
 // Quiz: "Writing a function type"
 // This quiz includes topics from these lessons: "Functions", "Function types". If you find this quiz difficult, revisiting those lessons may help!
 // Write a type for a function that takes a number and returns a string. For example, a function that takes a string and returns a number would have a type of (s: string) => number.
-
-type TakesNumberReturnsString = (num: number) => string
-
 // function add1AndAppend1(x: number): string {
 //   return (x + 1).toString() + 1
 // }
 // const f: TakesNumberReturnsString = add1AndAppend1
 // console.log(f(1))
-
-// function matchingFunc(n: number): string {
-//   return n.toString()
-// }
-
-// const p: TakesNumberReturnsString = matchingFunc
-
-// function add1AndAppend1(x: number): string {
-//   return (x + 1).toString() + 1;
-// }
-// const f: TakesNumberReturnsString = add1AndAppend1;
-// f('hello');
+function matchingFunc(n) {
+    return n.toString();
+}
+var p = matchingFunc;
