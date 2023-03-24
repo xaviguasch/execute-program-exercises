af3ead02d682f91983a1d1994c2f5ae5740f1884
Xavi Guasch <xaviguasch@gmail.com>
2023-03-24 17:18:19 +0100

    added t09-quiz-writing-a-function-type ts exercise

---
+++ typescript-basics/t09-quiz-writing-a-function-type/writing-a-function-type.ts
@@ -0,0 +1,16 @@
+// Quiz: "Writing a function type"
+// This quiz includes topics from these lessons: "Functions", "Function types". If you find this quiz difficult, revisiting those lessons may help!
+// Write a type for a function that takes a number and returns a string. For example, a function that takes a string and returns a number would have a type of (s: string) => number.
+
+type TakesNumberReturnsString = 
+
+
+
+
+
+
+// function add1AndAppend1(x: number): string {
+//   return (x + 1).toString() + 1;
+// }
+// const f: TakesNumberReturnsString = add1AndAppend1;
+// console.log(f(1))
