e551217533c18c1c96aebf3d5b5a5d752817485b
xaviguasch <xaviguasch@gmail.com>
2023-03-24 16:29:31 +0100

    added t08-quiz-destructuring-with-literal-objects ts challenge

---
+++ typescript-basics/t08-quiz-destructuring-with-literal-objects/destructuring-with-literal-objects.ts
@@ -0,0 +1,12 @@
+// Quiz: "Destructuring with literal objects"
+// This quiz includes topics from these lessons: "Functions", "Literal object types". If you find this quiz difficult, revisiting those lessons may help!
+// Write the types required to make this function type check. It takes an object with an email property of type string. It should return that email.
+
+function extractEmail({ email }: put_the_type_here): string {
+  return email
+}
+
+console.log(extractEmail({ email: 'amir@example.com' }))
+console.log(extractEmail({ email: 'betty@example.com' }))
+console.log(extractEmail({ address: 'cindy@example.com' }))
+console.log(extractEmail('dalili@example.com'))
