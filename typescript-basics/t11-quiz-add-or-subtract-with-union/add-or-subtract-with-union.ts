e96ac8a8533187a3bb94f4600179c4e71bd6b3a9
Xavi Guasch <xaviguasch@gmail.com>
2023-03-25 22:55:01 +0100

    added t11-quiz-add-or-subtract-with-union ts exercise

---
+++ typescript-basics/t11-quiz-add-or-subtract-with-union/add-or-subtract-with-union.ts
@@ -0,0 +1,14 @@
+// Quiz: "Add or subtract with union"
+// This quiz includes topics from these lessons: "Functions", "Literal types". If you find this quiz difficult, revisiting those lessons may help!
+// Write a function that adds 1, subtracts 1, or does nothing to a number. Argument 1 is the number. Argument 2 is 'add', 'subtract', or 'leave-alone'.
+
+function addOrSubtract() {
+  return
+}
+
+console.log(addOrSubtract(5, 'add'))
+console.log(addOrSubtract(5, 'subtract'))
+console.log(addOrSubtract(5, 'leave-alone'))
+console.log(addOrSubtract(5, 'increment'))
+console.log(addOrSubtract(5, null))
+console.log(addOrSubtract(5, true))
