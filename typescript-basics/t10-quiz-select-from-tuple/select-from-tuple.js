a789763f1c3ea3a57a385dc57c6c222062904d36
Xavi Guasch <xaviguasch@gmail.com>
2023-03-25 19:30:41 +0100

    added solution to previous challenge

--- typescript-basics/t10-quiz-select-from-tuple/select-from-tuple.js
+++ typescript-basics/t10-quiz-select-from-tuple/select-from-tuple.js
@@ -1,26 +1,19 @@
 // Quiz: "Select from tuple"
 // This quiz includes topics from these lessons: "Functions", "Arrays", "Tuples", "Type syntax is consistent". If you find this quiz difficult, revisiting those lessons may help!
 // Write a function that takes an array of tuples. Each tuple consists of a name (a string) and an age (a number). Your function should return only the names.
-
 // You'll need to use a loop. It can be a regular for loop, a call to .forEach, or a call to .map.
-
-function names(namesAndAges: [string, number][]): string[] {
-  return namesAndAges.map((tuple) => tuple[0])
+function names(namesAndAges) {
+    return namesAndAges.map(function (tuple) { return tuple[0]; });
 }
-
-console.log(
-  names([
+console.log(names([
     ['Amir', 34],
     ['Betty', 17],
-  ])
-)
-console.log(
-  names([
+]));
+console.log(names([
     ['Betty', 17],
     ['Cindy', 26],
     ['Dalili', 82],
     ['Ebony', 10],
-  ])
-)
+]));
 // console.log(names([['Ebony', '10']]))
 // console.log(names([['Feng', 'Tianwei', 32]]))
