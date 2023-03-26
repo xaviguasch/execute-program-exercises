d9b59004ce37ab863e0182c3033003e11f83434a
Xavi Guasch <xaviguasch@gmail.com>
2023-03-26 13:21:16 +0200

    added solution to previous challenge

--- typescript-basics/t13-generic-first-function/generic-first-function.js
+++ typescript-basics/t13-generic-first-function/generic-first-function.js
@@ -1,13 +1,11 @@
 // Quiz: "Generic first function"
 // This quiz includes topics from these lessons: "Functions", "Generic functions". If you find this quiz difficult, revisiting those lessons may help!
 // Write a function first<T> that returns the first element of an array. It should be generic, working with any element type.
-
-function first<T>(arr: Array<T>): T {
-  return arr[0]
+function first(arr) {
+    return arr[0];
 }
-
-console.log(first<number>([1, 2, 3]))
-console.log(first<string>(['a', 'b', 'c']))
-console.log(first<boolean>([false, true]))
+console.log(first([1, 2, 3]));
+console.log(first(['a', 'b', 'c']));
+console.log(first([false, true]));
 // console.log(first<number>(['a', 'b']))
 // console.log(first<string>('a'))
