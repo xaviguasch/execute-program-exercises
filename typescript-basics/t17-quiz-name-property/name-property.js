84b1380e75f5d37a9aaa5072e78883d51ef88120
Xavi Guasch <xaviguasch@gmail.com>
2023-03-26 16:20:49 +0200

    added t17-quiz-name-property ts exercise

---
+++ typescript-basics/t17-quiz-name-property/name-property.js
@@ -0,0 +1,13 @@
+// Quiz: "Name property"
+// This quiz includes topics from these lessons: "Functions", "Object narrowing". If you find this quiz difficult, revisiting those lessons may help!
+// Write a function that returns the name of an object. It should work for any object with a name: string property.
+function getName(_a) {
+    var name = _a.name;
+    return name;
+}
+var amir = { name: 'Amir', admin: true };
+console.log(getName(amir));
+var company = { name: 'My Awesome Company', companyType: 'LLC' };
+console.log(getName(company));
+// console.log(getName({ notAName: 'oh no' }))
+// console.log(getName(null))
