5faba7f5d1d7f94d7b97484385c0837807a4fc4c
Xavi Guasch <xaviguasch@gmail.com>
2023-03-26 15:50:48 +0200

    added solution to previous challenge

--- typescript-basics/t16-generic-first-with-narrowing/generic-first-with-narrowing.js
+++ typescript-basics/t16-generic-first-with-narrowing/generic-first-with-narrowing.js
@@ -1,18 +1,17 @@
 // Quiz: "Generic first with narrowing"
 // This quiz includes topics from these lessons: "Functions", "Generic functions", "Conditional narrowing". If you find this quiz difficult, revisiting those lessons may help!
 // Write a function first that returns the first element of an array. However, there are two twists. First, it should work for generic arrays, no matter the element type. Second, if we pass a single element, it should return just that element.
-
-function first<T>(el: T[] | T): T {
-  if (Array.isArray(el)) {
-    return el[0]
-  } else {
-    return el
-  }
+function first(el) {
+    if (Array.isArray(el)) {
+        return el[0];
+    }
+    else {
+        return el;
+    }
 }
-
-console.log(first([1, 2, 3]))
-console.log(first(['a', 'b', 'c']))
-console.log(first([true, false]))
-console.log(first<number>(1))
-console.log(first<string>('a'))
-console.log(first<boolean>(true))
+console.log(first([1, 2, 3]));
+console.log(first(['a', 'b', 'c']));
+console.log(first([true, false]));
+console.log(first(1));
+console.log(first('a'));
+console.log(first(true));
