9a2101dea6ce5b00515feeabfb00bbcb6d8c4e7e
Xavi Guasch <xaviguasch@gmail.com>
2023-03-26 13:08:19 +0200

    added solution to previous challenge

--- typescript-basics/t12-generic-identity-function/generic-identity-function.js
+++ typescript-basics/t12-generic-identity-function/generic-identity-function.js
@@ -1,13 +1,11 @@
 // Quiz: "Generic identity function"
 // This quiz includes topics from these lessons: "Functions", "Generic functions". If you find this quiz difficult, revisiting those lessons may help!
 // Write a generic identity function. (An identity function is one that returns its argument without changing it.)
-
-function identity<T>(element: T): T {
-  return element
+function identity(element) {
+    return element;
 }
-
-console.log(identity(1))
-console.log(identity('cat'))
-console.log(identity(true))
-console.log(identity(undefined))
-console.log(identity([1, 2]))
+console.log(identity(1));
+console.log(identity('cat'));
+console.log(identity(true));
+console.log(identity(undefined));
+console.log(identity([1, 2]));
