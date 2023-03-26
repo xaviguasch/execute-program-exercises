343ac3e74775601d81b87a5f4d1f0d9d8e48f0a1
Xavi Guasch <xaviguasch@gmail.com>
2023-03-26 13:32:54 +0200

    added solution to previous challenge

--- typescript-basics/t14-quiz-generic-length/generic-length.js
+++ typescript-basics/t14-quiz-generic-length/generic-length.js
@@ -1,13 +1,11 @@
 // Quiz: "Generic length"
 // This quiz includes topics from these lessons: "Functions", "Generic functions". If you find this quiz difficult, revisiting those lessons may help!
 // Write a function length<T> that returns the length of any array. It should be generic, working with any element type.
-
-function lengthFunc<T>(arr: Array<T>): number {
-  return arr.length
+function lengthFunc(arr) {
+    return arr.length;
 }
-
-console.log(lengthFunc([]))
-console.log(lengthFunc(['a']))
-console.log(lengthFunc([1, 2]))
-console.log(lengthFunc(['a']))
+console.log(lengthFunc([]));
+console.log(lengthFunc(['a']));
+console.log(lengthFunc([1, 2]));
+console.log(lengthFunc(['a']));
 // console.log(lengthFunc(undefined))
