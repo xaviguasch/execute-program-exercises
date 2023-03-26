4e9c217d95d760a678c4748c7ba3e8c620a9c445
Xavi Guasch <xaviguasch@gmail.com>
2023-03-26 13:50:40 +0200

    added t15-quiz-has-logged-in ts exercise

---
+++ typescript-basics/t15-quiz-has-logged-in/has-logged-in.ts
@@ -0,0 +1,15 @@
+// Quiz: "Has logged in"
+// This quiz includes topics from these lessons: "Functions", "Conditional narrowing", "Literal types". If you find this quiz difficult, revisiting those lessons may help!
+// Write a function that decides whether a user has logged in. Sometimes, it's called with a number of times that the user has logged in. Other times, it's called with true. It's never called with false.
+
+function hasLoggedIn() {
+  return
+}
+
+console.log(hasLoggedIn(true))
+console.log(hasLoggedIn(0))
+console.log(hasLoggedIn(1))
+console.log(hasLoggedIn(5))
+console.log(hasLoggedIn(-1))
+console.log(hasLoggedIn(false))
+console.log(hasLoggedIn(undefined))
