819e50f0ff1de4d2b2b3ce0c6626ea3c7d873d87
Xavi Guasch <xaviguasch@gmail.com>
2023-03-26 16:35:25 +0200

    added solution to previous challenge

--- typescript-basics/t18-object-type-with-holes/object-type-with-holes.js
+++ typescript-basics/t18-object-type-with-holes/object-type-with-holes.js
@@ -1,32 +1,23 @@
 // Quiz: "Object type with holes"
 // This quiz includes topics from these lessons: "Functions", "Generic object types". If you find this quiz difficult, revisiting those lessons may help!
 // Write a type for Pants. left and right should be able to hold any types.
-
-type Pants<T1, T2> = {
-  left: T1
-  right: T2
-}
-
 // let myPants: Pants<string, number> = {
 //   left: 'phone',
 //   right: 5,
 // }
 // myPants.left
-
 // let myPants: Pants<boolean, boolean> = {
 //   left: false,
 //   right: true,
 // }
 // myPants.right
-
 // let myPants: Pants<number, string> = {
 //   left: 5,
 //   right: 'phone',
 // }
 // myPants.left
-
-// let myPants: Pants = {
-//   left: 'phone',
-//   right: 'wallet',
-// }
-// myPants.right
+var myPants = {
+    left: 'phone',
+    right: 'wallet',
+};
+myPants.right;
