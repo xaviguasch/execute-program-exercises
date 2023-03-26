da73f4c7c86efd8fcf915eda96aad61f4f5008cf
Xavi Guasch <xaviguasch@gmail.com>
2023-03-26 14:15:45 +0200

    added t16-quiz-generic-first-with-narrowing ts exercise

--- typescript-basics/t15-quiz-has-logged-in/has-logged-in.js
+++ typescript-basics/t15-quiz-has-logged-in/has-logged-in.js
@@ -1,26 +1,25 @@
 // Quiz: "Has logged in"
 // This quiz includes topics from these lessons: "Functions", "Conditional narrowing", "Literal types". If you find this quiz difficult, revisiting those lessons may help!
 // Write a function that decides whether a user has logged in. Sometimes, it's called with a number of times that the user has logged in. Other times, it's called with true. It's never called with false.
-
-function hasLoggedIn<T>(action: true | number): boolean {
-  if (typeof action === 'number' && action > 0) {
-    return true
-  } else if (action === true) {
-    return true
-  } else {
-    return false
-  }
+function hasLoggedIn(action) {
+    if (typeof action === 'number' && action > 0) {
+        return true;
+    }
+    else if (action === true) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-
 // alternative solution
 // function hasLoggedIn(times: number | true) {
 //   return (typeof times === 'boolean') ? true : times > 0;
 // }
-
-console.log(hasLoggedIn(true))
-console.log(hasLoggedIn(0))
-console.log(hasLoggedIn(1))
-console.log(hasLoggedIn(5))
+console.log(hasLoggedIn(true));
+console.log(hasLoggedIn(0));
+console.log(hasLoggedIn(1));
+console.log(hasLoggedIn(5));
 // console.log(hasLoggedIn(-1))
 // console.log(hasLoggedIn(false))
 // console.log(hasLoggedIn(undefined))
---
+++ typescript-basics/t16-generic-first-with-narrowing/generic-first-with-narrowing.ts
@@ -0,0 +1,14 @@
+// Quiz: "Generic first with narrowing"
+// This quiz includes topics from these lessons: "Functions", "Generic functions", "Conditional narrowing". If you find this quiz difficult, revisiting those lessons may help!
+// Write a function first that returns the first element of an array. However, there are two twists. First, it should work for generic arrays, no matter the element type. Second, if we pass a single element, it should return just that element.
+
+function first() {
+  return
+}
+
+console.log(first([1, 2, 3]))
+console.log(first(['a', 'b', 'c']))
+console.log(first([true, false]))
+console.log(first<number>(1))
+console.log(first<string>('a'))
+console.log(first<boolean>(true))
