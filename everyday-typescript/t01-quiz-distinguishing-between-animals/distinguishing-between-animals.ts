f04be9e84d0c8b5a85ba6880f5916bb23f7e4f45
Xavi Guasch <xaviguasch@gmail.com>
2023-03-29 17:14:32 +0200

    added solution to previous challenge

--- everyday-typescript/t01-quiz-distinguishing-between-animals/distinguishing-between-animals.ts
+++ everyday-typescript/t01-quiz-distinguishing-between-animals/distinguishing-between-animals.ts
@@ -2,8 +2,8 @@
 // This quiz includes topics from this lesson: "Discriminated unions". If you find this quiz difficult, revisiting that lesson may help!
 // Cats and dogs are both animals, but cats meow and dogs bark. Fill in the Cat and Dog type definitions to make the tests pass.
 
-type Cat = {}
-type Dog = {}
+type Cat = { kind: 'cat'; meow: () => string }
+type Dog = { kind: 'dog'; bark: () => string }
 type Animal = Cat | Dog
 
 // const animal: Animal = {kind: 'cat', meow: () => 'meow'};
