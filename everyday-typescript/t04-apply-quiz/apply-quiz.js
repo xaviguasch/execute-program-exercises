c07b2771a1d65baca38ba14943e5f731410d69ca
Xavi Guasch <xaviguasch@gmail.com>
2023-09-02 18:17:15 +0200

    added solution to previous challenge

--- everyday-typescript/t04-apply-quiz/apply-quiz.js
+++ everyday-typescript/t04-apply-quiz/apply-quiz.js
@@ -1,10 +1,8 @@
 // Quiz: "Apply function"
 // This quiz includes topics from this lesson: "Functions as arguments". If you find this quiz difficult, revisiting that lesson may help!
 // Write a function applyFunction that takes two arguments. The first argument is a function f, which itself takes a number and returns a number. The second argument to applyFunction is a number, x. applyFunction should return f(x). We've provided the function body, but the argument types are missing.
-
-function applyFunction(f: (num: number) => number, x: number) {
-  return f(x)
+function applyFunction(f, x) {
+    return f(x);
 }
-
-console.log(applyFunction((n) => n + 1, 2))
-console.log(applyFunction((n) => n * 10, 3))
+console.log(applyFunction(function (n) { return n + 1; }, 2));
+console.log(applyFunction(function (n) { return n * 10; }, 3));
