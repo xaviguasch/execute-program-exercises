6c6fd2672b01e2a6780d3b21fba1b3392a242553
Xavi Guasch <xaviguasch@gmail.com>
2023-09-17 18:40:28 +0200

    added quiz-add-exclamation challenge for js-arrays

---
+++ javascript-arrays/quiz-add-exclamation.js
@@ -0,0 +1,10 @@
+// Quiz: "Add exclamation"
+// This quiz includes topics from this lesson: "Map". If you find this quiz difficult, revisiting that lesson may help!
+// Use .map to write a function that appends a '!' to every element.
+
+function excite(strings) {
+  return strings
+}
+
+console.log(excite(['a', 'b', 'c']))
+console.log(excite(['A cat', 'And a dog']))
