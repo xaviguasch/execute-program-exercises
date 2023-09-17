bb4acc81fff74747a2cb113ff452a798098a9cab
Xavi Guasch <xaviguasch@gmail.com>
2023-09-17 17:02:38 +0200

    added quiz-empty exercise for js-arrays

---
+++ javascript-arrays/quiz-empty.js
@@ -0,0 +1,10 @@
+// Quiz: "Empty"
+// This quiz includes topics from this lesson: "Basics". If you find this quiz difficult, revisiting that lesson may help!
+// Write a function isEmpty(arr) that returns true if the array is empty.
+
+function isEmpty(arr) {
+  return true
+}
+
+console.log(isEmpty([]))
+console.log(isEmpty([1, 2]))
