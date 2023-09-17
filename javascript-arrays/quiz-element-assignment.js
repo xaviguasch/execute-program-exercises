f2b037cf0cb91c7a67670a6475052aa7556bf998
Xavi Guasch <xaviguasch@gmail.com>
2023-09-17 17:22:46 +0200

    added quiz-element-assignment for js-arrays

---
+++ javascript-arrays/quiz-element-assignment.js
@@ -0,0 +1,13 @@
+// Quiz: "Element assignment"
+// This quiz includes topics from this lesson: "Basics". If you find this quiz difficult, revisiting that lesson may help!
+// Write a function that puts a 0 in element 1 of an array. Remember that arrays are zero-indexed.
+
+// Make sure that the function returns undefined. Returning undefined indicates that the function modifies some data: either its arguments or global variables.
+
+// (If a function returns nothing, but also modifies nothing, then it does nothing at all! It must return something, modify something, or both. By returning undefined, we tell callers of our function that it modifies something.)
+
+function setSecondElementToZero(nums) {}
+
+const array = [0, 0, 0]
+
+console.log(setSecondElementToZero(array))
