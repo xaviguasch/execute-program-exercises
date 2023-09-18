db49b45ddb62fc7d503a9fe5f4dada59dbc34020
Xavi Guasch <xaviguasch@gmail.com>
2023-09-18 11:43:17 +0200

    added quiz-get-first-elements for js-arrays

---
+++ javascript-arrays/quiz-get-first-elements.js
@@ -0,0 +1,13 @@
+// Quiz: "Get first elements"
+// This quiz includes topics from this lesson: "Slice". If you find this quiz difficult, revisiting that lesson may help!
+// Use .slice to write a function takeFirst(arr, n). It should return a new array of the first n elements of arr. If n is larger than the array's length, it should return a copy of the array.
+
+// Don't worry about negative n.
+
+function takeFirst(arr, n) {
+  return []
+}
+
+console.log(takeFirst([1, 2, 3], 0))
+console.log(takeFirst([1, 2, 3], 1))
+console.log(takeFirst([1, 2, 3], 2))
