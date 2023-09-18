8e560da28aaf201fcbdbe1335cb2d8cf748ef31c
Xavi Guasch <xaviguasch@gmail.com>
2023-09-18 11:48:15 +0200

    added quiz-copy-array for js-arrays

---
+++ javascript-arrays/quiz-copy-array.js
@@ -0,0 +1,10 @@
+// Quiz: "Copy array"
+// This quiz includes topics from this lesson: "Slice". If you find this quiz difficult, revisiting that lesson may help!
+// Write a function copyArray that returns a copy of an array. Changes to the copy shouldn't affect the original array and vice-versa.
+
+function copyArray(array) {
+  return array
+}
+
+console.log(copyArray(['a', 'b']))
+console.log(copyArray(['a', 'b']))
